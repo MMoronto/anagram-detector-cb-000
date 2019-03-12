cdf91a4bbffbddd43667873828f4ba3cc727e23e
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-03-12 21:09:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,22 +1,8 @@
 class Anagram
   attr_accessor :words 
   
-  @@all = [] 
-  
-  def self.all
-    @@all
-  end 
-  
-  def self.create(words)
-    anagram = self.new 
-    anagram.words = words
-    @@all << anagram 
-  end 
-  
-  def self.match(array)
-    self.all.each{|anagram| 
-      anagram.
-    }
+  def initialize(word)
+    @word = word
   end 
   
 end 
